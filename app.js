c28105b9abebccff2afe228b144798f56f2ae688
Mateus <144482173+Mateuscde@users.noreply.github.com>
2024-01-27 16:19:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const video = document.addEventListener('DOMContentLoaded', function () {
     const video = document.getElementById('preview');
     const scanner = new Instascan.Scanner({ video: video });
-    var n=0;
+    
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
             scanner.start(cameras[0]);
 
             scanner.addListener('scan', function (content) {
-                n=content;
-                alert(n);
-               // if(content=="CP-0000"){alert("oxion vulop")};
+               alert(content);
+               if(content=="CP-0000"){alert("oxion vulop")};
             });
         } else {
             console.error('No cameras found.');
@@ -31,9 +30,8 @@ document.addEventListener('DOMContentLoaded', function () {
             scanner.start(cameras[0]);
 
             scanner.addListener('scan', function (content) {
-                n=content;
-                alert(n);
-                //if(content=="CP-0000"){alert("oxion vulop")};
+                alert(content);
+                if(content=="CP-0000"){alert("oxion vulop")};
             });
         } else {
             console.error('No cameras found.');
