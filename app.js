6a3ff84036590443a1cbf9cab95ea24045712215
Mateus <144482173+Mateuscde@users.noreply.github.com>
2024-01-27 15:22:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const video = document.getElementById('preview');
+    const scanner = new Instascan.Scanner({ video: video });
+
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+
+            scanner.addListener('scan', function (content) {
+                alert('QR code lido: ' + content);
+            });
+        } else {
+            console.error('No cameras found.');
+        }
+    }).catch(function (e) {
+        console.error(e);
+    });
+
+    const qrCodeValue = 'Hello, World!'; // Valor do QR code que voc¨º deseja gerar
+    const qrcode = new QRCode(document.getElementById('qrcode'), qrCodeValue);
+});
